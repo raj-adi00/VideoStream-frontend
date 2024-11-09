cb03f95768361028b8ecc6d8a1f8a05738062885
Aditya Raj <adi.adityakohli2005@gmail.com>
2024-11-09 16:02:18 +0530

    added funciton to visit anyone channel page

--- src/Components/Frequent/Channel.jsx
+++ src/Components/Frequent/Channel.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { info } from '../../Store/ErrorMessageSlice';
+import { useNavigate } from 'react-router-dom';
 
 function Channel({ ...props }) {
     const { username, createdAt } = props.channelDetails || {};
-
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const handleclick = () => {
+        if (username) {
+            navigate(`/u/${username}`)
+        } else {
+            dispatch(info('Username not found'))
+        }
+    }
     return (
         <div className="flex flex-col sm:flex-row justify-between items-center my-3 p-3 border bg-blue-300 border-gray-300 rounded-md shadow-sm w-full max-w-lg">
-            <div className="text-lg font-semibold text-gray-800">
+            <div className="text-lg font-semibold text-gray-800" onClick={handleclick}>
                 <span className="font-bold text-gray-900">Channel: </span>
                 {username || 'Unknown'}
             </div>
@@ -17,7 +28,7 @@ function Channel({ ...props }) {
                         year: 'numeric',
                         month: 'long',
                         day: 'numeric',
-                      })
+                    })
                     : 'Date not available'}
             </div>
         </div>
--- src/Components/Frequent/ChannelName.jsx
+++ src/Components/Frequent/ChannelName.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { info } from '../../Store/ErrorMessageSlice'
 function ChannelName() {
+    const navigate = useNavigate()
     const user = useSelector((state) => state.auth.userDetails)
+    const dispatch = useDispatch()
+    const handlClick = () => {
+        const username = user?.username
+        if (username)
+            navigate(`/u/${username}`)
+        else {
+            dispatch(info('Username not found'))
+        }
+    }
     return (
-        <div className="text-lg font-semibold text-gray-800">
+        <div className="text-lg font-semibold text-gray-800" onClick={handlClick}>
             <span className="font-bold text-gray-900">Channel: </span>
-            {user?.data?.username || 'Unknown'}
+            {user?.username || 'Unknown'}
         </div>
     )
 }
