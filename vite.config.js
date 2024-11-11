3afee60446dbb070e3731c4a288ef0d02b7cf235
Aditya Raj <adi.adityakohli2005@gmail.com>
2024-11-11 11:35:59 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-   proxy:{
-    '/api':'https://itube-iser.onrender.com'
-   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://itube-iser.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '') // removes '/api' prefix
+      }
+    },
   },
   plugins: [react()],
 })
