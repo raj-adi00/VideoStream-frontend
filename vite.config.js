80ba12a5c76235c18bba32a24591e6ae9694ac07
Aditya Raj <adi.adityakohli2005@gmail.com>
2024-11-11 11:32:46 +0530

    updated proxy slash

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
    proxy:{
-    '/api':'https://itube-iser.onrender.com/'
+    '/api':'https://itube-iser.onrender.com'
    },
   },
   plugins: [react()],
