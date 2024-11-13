d17e230b8cb7e8d56fdf0608d3c4948b4e782eb4
Aditya Raj <adi.adityakohli2005@gmail.com>
2024-11-13 13:00:44 +0530

    updated for test

--- src/Components/Interceptor/apiClient.js
+++ src/Components/Interceptor/apiClient.js
@@ -45,7 +45,7 @@ const response_interceptor = axios.create({
     withCredentials: true,
 });
 const apiClient2 = axios.create({
-    baseURL: 'http://localhost:8000/api/v1/videos',
+    baseURL: 'https://itube-iser.onrender.com/api/v1/videos',
     withCredentials: true,
 });
 
