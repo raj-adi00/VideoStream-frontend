7ca11ad31fe1b63dda438b4aaa7b328bf14cc1d7
Aditya Raj <adi.adityakohli2005@gmail.com>
2024-11-28 01:16:44 +0530

    updated css of thumbnail image

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -37,7 +37,7 @@ const Card = ({ _id, thumbnail, title, owner, channel_owner, video_public_id, to
     return (
         <div className="w-80 rounded-lg overflow-hidden shadow-lg bg-white dark:bg-gray-800 dark:text-gray-200 border border-gray-300 dark:border-gray-700 min-w-80 min-h-72 transition duration-300 ease-in-out hover:shadow-xl">
             <Link state={{ video_public_id }} onClick={updateViews}>
-                <img className="w-full h-60 object-cover" src={thumbnail} alt={title} />
+                <img className="w-full h-60" src={thumbnail} alt={title} />
             </Link>
             <div className="px-3 py-4 overflow-hidden">
                 <div className="font-semibold text-base text-left mb-2 truncate text-gray-900 dark:text-gray-100">
