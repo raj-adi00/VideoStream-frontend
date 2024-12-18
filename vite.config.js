3985fb6a38387491989bc64d7492208fa510d0eb
Aditya Raj <adi.adityakohli2005@gmail.com>
2024-12-18 15:07:42 +0530

    updated proxy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://itube-iser.onrender.com'
+      // '/api': 'https://itube-iser.onrender.com'
       // '/api':'http://localhost:8000'
+      "/api": "videostream-production-3c23.up.railway.app",
     },
   },
   plugins: [react()],
-})
+});
