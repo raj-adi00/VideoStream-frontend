a1c939aec66fa5f37ce299c4abb2b18dd0dff401
Aditya Raj <adi.adityakohli2005@gmail.com>
2025-02-10 01:19:27 +0530

    ..

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./Components/Home.jsx";
+import Home from "./Components/Home";
 import { Provider } from "react-redux";
 import store from "./Store/store.js";
-import Register from "./Components/Register.jsx";
-import Login from "./Components/Login.jsx";
-import VideoPage from "./Components/VideoPage.jsx";
-import UploadVideoForm from "./Components/UploadVideoForm.jsx";
-import Tweet from "./Components/Tweet/Tweet.jsx";
-import ChatComponent from "./Components/Chat/Chatcomponent.jsx";
-import MyVideo from "./Components/My Video/MyVideo.jsx";
-import UserPage from "./Components/User/UserPage.jsx";
-import ChatLoadingPage from "./Components/Chat/ChatLoadingPage.jsx";
-import AddFriend from "./Components/Chat/AddFriend.jsx";
+import Register from "./Components/Register";
+import Login from "./Components/Login";
+import VideoPage from "./Components/VideoPage";
+import UploadVideoForm from "./Components/UploadVideoForm";
+import Tweet from "./Components/Tweet/Tweet";
+import ChatComponent from "./Components/Chat/Chatcomponent";
+import MyVideo from "./Components/My Video/MyVideo";
+import UserPage from "./Components/User/UserPage";
+import ChatLoadingPage from "./Components/Chat/ChatLoadingPage";
+import AddFriend from "./Components/Chat/AddFriend";
 
 const router = createBrowserRouter([
   {
