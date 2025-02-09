709bbe069be3d6da7b47c03e6129dd87965539ca
Aditya Raj <adi.adityakohli2005@gmail.com>
2025-02-10 01:31:46 +0530

    ..

--- src/main.jsx
+++ src/main.jsx
@@ -34,7 +34,7 @@ const router = createBrowserRouter([
         element: <Login />,
       },
       {
-        path: "/video/:id",
+        path: "/:id",
         element: <VideoPage />,
       },
       {
