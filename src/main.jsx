56b5e1ca2578754265a7865d4cb32ca8a18d7a00
Aditya Raj <adi.adityakohli2005@gmail.com>
2025-02-10 01:16:06 +0530

    ..

--- src/main.jsx
+++ src/main.jsx
@@ -70,11 +70,9 @@ const router = createBrowserRouter([
   },
 ]);
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
-  <Provider store={store}>
-    <RouterProvider router={router} />
-  </Provider>,
-  {
-    /* </StrictMode> */
-  }
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
 );
